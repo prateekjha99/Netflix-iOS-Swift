ee75d842b410f26f1fe6b584ebca76b5e263fa29
prateekjha99 <prateekjhawhiteteeth@gmail.com>
2026-04-23 14:33:04 +0530

    Initial commit

---
+++ Netflix iOS Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix iOS Swift
+//
+//  Created by Prateek Jha on 23/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
