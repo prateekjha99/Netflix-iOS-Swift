50b76182b9b171aa7d39b2ff63bb80cc61da3122
prateekjha99 <prateekjhawhiteteeth@gmail.com>
2026-04-25 17:37:06 +0530

    Add hero image assets and implement HeroHeaderUIView in HomeViewController

--- Netflix iOS Swift/Controllers/HomeViewController.swift
+++ Netflix iOS Swift/Controllers/HomeViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    private var headerView: HeroHeaderUIView?
     
     private let homeFeedTable: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
@@ -24,7 +25,8 @@ class HomeViewController: UIViewController {
         homeFeedTable.delegate = self
         homeFeedTable.dataSource = self
         
-        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+        homeFeedTable.tableHeaderView = headerView
     }
     
     
