b18c8043923472a902e39ec8f7834481f0ee9c79
prateekjha99 <prateekjhawhiteteeth@gmail.com>
2026-04-26 13:07:39 +0530

    Add TitleCollectionViewCell for displaying movie posters; update CollectionViewTableViewCell to use new cell and configure data retrieval for various sections in HomeViewController

--- Netflix iOS Swift/Controllers/HomeViewController.swift
+++ Netflix iOS Swift/Controllers/HomeViewController.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+
+
+enum Sections: Int {
+    case TrendingMovies = 0
+    case TrendingTv = 1
+    case Popular = 2
+    case Upcoming = 3
+    case TopRated = 4
+}
+
+
+
 class HomeViewController: UIViewController {
     
     private var headerView: HeroHeaderUIView?
@@ -74,9 +86,66 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath)
                 as? CollectionViewTableViewCell else {
-                return UITableViewCell()
+            return UITableViewCell()
         }
+        
+        switch indexPath.section {
+            case Sections.TrendingMovies.rawValue:
+                APICaller.shared.getTrendingMovies { result in
+                    switch result {
+                        
+                    case .success(let titles):
+                        print(titles)
+                        cell.configure(with: titles)
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
                 
+                
+                
+            case Sections.TrendingTv.rawValue:
+                APICaller.shared.getTrendingTvs { result in
+                    switch result {
+                    case .success(let titles):
+                        cell.configure(with: titles)
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
+            case Sections.Popular.rawValue:
+                APICaller.shared.getPopular { result in
+                    switch result {
+                    case .success(let titles):
+                        cell.configure(with: titles)
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
+            case Sections.Upcoming.rawValue:
+                
+                APICaller.shared.getUpcomingMovies { result in
+                    switch result {
+                    case .success(let titles):
+                        cell.configure(with: titles)
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
+                
+            case Sections.TopRated.rawValue:
+                APICaller.shared.getTopRated { result in
+                    switch result {
+                    case .success(let titles):
+                        cell.configure(with: titles)
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+        default:
+            return UITableViewCell()
+        }
+
         return cell
     }
     
